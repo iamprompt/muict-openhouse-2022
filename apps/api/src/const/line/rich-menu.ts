5125b14764fcb0c076c84b1e13597f78130ba845
Supakarn Laorattanakul <wearedprompt@gmail.com>
2022-11-20 23:46:54 +0700

    fix prod register richmenu

--- apps/api/src/const/line/rich-menu.ts
+++ apps/api/src/const/line/rich-menu.ts
@@ -1,7 +1,7 @@
 export const RICH_MENU_BY_ENV = {
   production: {
     DEFAULT: 'richmenu-118cf162f175ff0a6e7833f164022607',
-    REGISTERED: 'richmenu-b876c8dbbf160009e38c78101100f046',
+    REGISTERED: 'richmenu-b6f29a5aceb280157af244b50d8d38f7',
   },
   development: {
     DEFAULT: 'richmenu-118cf162f175ff0a6e7833f164022607',
